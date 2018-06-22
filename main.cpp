e6c7bc309e51a389710a0a164744a5089a4bd9f4
Tihomir Kovachev <tihomir.kovachev@egt-bg.com>
2018-06-22 10:00:50 +0300

    All functions without iterator implemented.

--- main.cpp
+++ main.cpp
@@ -7,19 +7,19 @@ int main()
     Vector<int> myVector(array, 4, 2);
 
     /** adding elements into vector */
-    myVector.Push_back(6);
-    myVector.Push_back(3);
-    myVector.Push_back(7);
-    myVector.Push_back(1);
-    myVector.Push_back(5);
+    myVector.push_back(6);
+    myVector.push_back(3);
+    myVector.push_back(7);
+    myVector.push_back(1);
+    myVector.push_back(6);
 
     /** inserting element at position */
-    myVector.Insert(10, 3);
-    myVector.Insert(54, 5);
+    myVector.insert(10, 3);
+    myVector.insert(54, 5);
 
     /** print the vector */
     cout << "Vector elements: ";
-    myVector.Print();
+    myVector.print();
     cout << endl;
 
     /** Creating new vector with that will be a copy of the one above, in this case we call the Copy Constructor */
@@ -27,7 +27,7 @@ int main()
 
     /** Print copied vector */
     cout << "Copied vector elements: ";
-    copyVector.Print();
+    copyVector.print();
     cout << endl;
 
     /** Creating new vector with assignment operator, almost the same as the Copy Constructor */
@@ -35,38 +35,38 @@ int main()
 
     /** Print assignment vector */
     cout << "Assignment vector elements: ";
-    assignmentVector.Print();
+    assignmentVector.print();
     cout << endl;
 
     /** first and last element of vector */
-    cout << "First element: " << assignmentVector.Front() << "\nLast element: " << assignmentVector.Back() << endl;
+    cout << "First element: " << assignmentVector.front() << "\nLast element: " << assignmentVector.back() << endl;
 
     /** remove last element of vector, in this case we remove 5 */
-    assignmentVector.Pop_back();
+    assignmentVector.pop_back();
     cout << "New vector elements: ";
-    assignmentVector.Print();
+    assignmentVector.print();
     cout << endl;
 
     /** remove element at position, in this case we remove 3 on fourth position */
-    assignmentVector.Erase(4);
+    assignmentVector.erase(4);
     cout << "New vector elements: ";
-    assignmentVector.Print();
+    assignmentVector.print();
     cout << endl;
 
     /** sort vector */
-    assignmentVector.Sort();
+    assignmentVector.sort();
 
     /** print sorted vector */
     cout << "Sorted vector elements: ";
-    assignmentVector.Print();
+    assignmentVector.print();
     cout << endl;
 
     /** clear vector */
-    assignmentVector.Clear();
+    assignmentVector.clear();
 
     /** print empty vector*/
-    cout << "Cleared vector: " << (assignmentVector.IsEmpty() == true ? "yes " : "false ");
-    assignmentVector.Print();
+    cout << "Cleared vector: " << (assignmentVector.empty() == true ? "yes " : "false ");
+    assignmentVector.print();
     cout << endl;
 
     return 0;
--- vector.h
+++ vector.h
@@ -14,53 +14,63 @@ public:
     Vector(const TYPE* data, const size_t size, const size_t capacity);
     Vector(const Vector& other);
     Vector& operator =(const Vector& other);
-    TYPE& operator[](const size_t& idx);
+    TYPE& operator [](const size_t& idx);
+    bool operator ==(const Vector& other);
+    bool operator !=(const Vector& other);
+    bool operator <(const Vector& other);
+    bool operator >(const Vector& other);
+    bool operator <=(const Vector& other);
+    bool operator >=(const Vector& other);
     ~Vector();
 
 // the public methods
 public:
-    void Push_back(const TYPE& data);
-    void Pop_back();
-    void Insert(const TYPE& data, const size_t& position);
-    void Erase(const size_t& position);
-    void Sort();
-    void Clear();
+    void assign(const size_t& count, const TYPE& value);
+    void push_back(const TYPE& data);
+    void pop_back();
+    void insert(const TYPE& data, const size_t& position);
+    void erase(const size_t& position);
+    void sort();
+    void clear();
+    void shrink_to_fit();
+    void swap( Vector& other );
 
-    size_t GetSize() const;
-    size_t GetCapacity() const;
-    bool IsEmpty() const;
-    TYPE& Front() const;
-    TYPE& Back() const;
-    void Print() const;
+    TYPE& at(const size_t& pos) const;
+    size_t size() const;
+    size_t capacity() const;
+    bool empty() const;
+    TYPE& front() const;
+    TYPE& back() const;
+    void print() const;
 
 // the private methods
 private:
-    void Copy(const Vector& other);
-    void Resize(const size_t& newSize);
+    void copy(const Vector& other);
+    void resize(const size_t& newCapacity);
 
 // the private members
 private:
     TYPE* m_data;
-    size_t m_size;
     size_t m_capacity;
+    size_t m_size;
 };
 
 // the public methods
 template <class TYPE>
 Vector<TYPE>::Vector()
 {
-    m_data = nullptr;
-    m_size = 0;
     m_capacity = 0;
+    m_size = 0;
+    m_data = new TYPE[m_capacity];
 }
 
 template <class TYPE>
 Vector<TYPE>::Vector(const TYPE* data, const size_t size, const size_t capacity)
 {
-    m_size = size;
     m_capacity = capacity;
-    m_data = new TYPE[m_size];
-    for(size_t i = 0; i < capacity; i++)
+    m_size = size;
+    m_data = new TYPE[m_capacity];
+    for(size_t i = 0; i < size; i++)
     {
         m_data[i] = data[i];
     }
@@ -71,7 +81,7 @@ Vector<TYPE>::Vector(const Vector& other)
 {
     if(this != &other)
     {
-        Copy(other);
+        copy(other);
     }
 }
 
@@ -80,9 +90,8 @@ Vector<TYPE>& Vector<TYPE>::operator=(const Vector& other)
 {
     if(this != &other)
     {
-        Copy(other);
+        copy(other);
     }
-
     return *this;
 }
 
@@ -93,56 +102,137 @@ TYPE& Vector<TYPE>::operator [](const size_t& idx)
 }
 
 template <class TYPE>
+bool Vector<TYPE>::operator ==(const Vector& other)
+{
+    if(m_capacity == other.m_capacity && m_size == other.m_size)
+    {
+        for(size_t i = 0; i < m_size; i++)
+        {
+            if(m_data[i] != other.m_data[i])
+            {
+                return false;
+            }
+        }
+        return true;
+    }
+    return false;
+}
+
+template <class TYPE>
+bool Vector<TYPE>::operator !=(const Vector& other)
+{
+    return !(operator ==(other));
+}
+
+template <class TYPE>
+bool Vector<TYPE>::operator <(const Vector& other)
+{
+    if(m_capacity < other.m_capacity || m_size < other.m_size)
+    {
+        return true;
+    }
+    else if(m_capacity == other.m_capacity && m_size == other.m_size)
+    {
+        for(size_t i = 0; i < m_size; i++)
+        {
+            if(m_data[i] < other.m_data[i])
+            {
+                return true;
+            }
+        }
+    }
+    return false;
+}
+
+template <class TYPE>
+bool Vector<TYPE>::operator >(const Vector& other)
+{
+    if(m_capacity > other.m_capacity || m_size > other.m_size)
+    {
+        return true;
+    }
+    else if(m_capacity == other.m_capacity && m_size == other.m_size)
+    {
+        for(size_t i = 0; i < m_size; i++)
+        {
+            if(m_data[i] > other.m_data[i])
+            {
+                return true;
+            }
+        }
+    }
+    return false;
+}
+
+template <class TYPE>
+bool Vector<TYPE>::operator <=(const Vector& other)
+{
+    return !(operator >(other));
+}
+
+template <class TYPE>
+bool Vector<TYPE>::operator >=(const Vector& other)
+{
+    return !(operator <(other));
+}
+
+template <class TYPE>
 Vector<TYPE>::~Vector()
 {
     delete[] m_data;
 }
 
 template <class TYPE>
-void Vector<TYPE>::Push_back(const TYPE& data)
+void Vector<TYPE>::assign(const size_t& count, const TYPE& value)
 {
-    if(m_capacity >= m_size)
+    m_size = count;
+    m_capacity = count * 2;
+    for(size_t i = 0; i < m_size; i++)
     {
-        Resize(m_size * 2);
+        m_data[i] = value;
     }
-
-    m_data[m_capacity] = data;
-    m_capacity++;
 }
 
 template <class TYPE>
-void Vector<TYPE>::Pop_back()
+void Vector<TYPE>::push_back(const TYPE& data)
 {
-    TYPE* newData = new TYPE[m_capacity - 1];
+    if(m_size >= m_capacity)
+    {
+        resize(m_capacity * 2);
+    }
+    m_data[m_size] = data;
+    m_size++;
+}
 
-    for(size_t i = 0; i < m_capacity - 1; i++)
+template <class TYPE>
+void Vector<TYPE>::pop_back()
+{
+    TYPE* newData = new TYPE[m_capacity];
+    for(size_t i = 0; i < m_size - 1; i++)
     {
         newData[i] = m_data[i];
     }
-
     delete[] m_data;
     m_data = newData;
-    m_capacity--;
+    m_size--;
 }
 
 template <class TYPE>
-void Vector<TYPE>::Insert(const TYPE& data, const size_t& position)
+void Vector<TYPE>::insert(const TYPE& data, const size_t& position)
 {
-    if(position < 0 || position > m_capacity)
+    if(position < 0 || position > m_size)
     {
         cerr << "Invalid position" << endl;
         return;
     }
     else
     {
-        if(m_capacity >= m_size)
+        if(m_size >= m_capacity)
         {
-            Resize(m_size * 2);
+            resize(m_capacity * 2);
         }
-
-        TYPE* newData = new TYPE[m_size];
-
-        for(size_t i = 0; i <= m_capacity; i++)
+        TYPE* newData = new TYPE[m_capacity];
+        for(size_t i = 0; i <= m_size; i++)
         {
             if(i < position)
             {
@@ -157,44 +247,40 @@ void Vector<TYPE>::Insert(const TYPE& data, const size_t& position)
                 newData[i] = m_data[i - 1];
             }
         }
-
         delete[] m_data;
         m_data = newData;
-        m_capacity++;
+        m_size++;
     }
 }
 
 template <class TYPE>
-void Vector<TYPE>::Erase(const size_t& position)
+void Vector<TYPE>::erase(const size_t& position)
 {
-    if(position < 0 || position > m_capacity)
+    if(position < 0 || position > m_size)
     {
         cerr << "Invalid position" << endl;
         return;
     }
-
-    for(size_t i = 0; i < m_capacity; i++)
+    for(size_t i = 0; i < m_size; i++)
     {
         if(i == position)
         {
-            for(size_t j = i; j < m_capacity; j++)
+            for(size_t j = i; j < m_size; j++)
             {
                 m_data[j] = m_data[j + 1];
             }
         }
     }
-
-    m_capacity--;
+    m_size--;
 }
 
 template <class TYPE>
-void Vector<TYPE>::Sort()
+void Vector<TYPE>::sort()
 {
     TYPE temp;
-
-    for(size_t i = 0; i < m_capacity; i++)
+    for(size_t i = 0; i < m_size; i++)
     {
-        for(size_t j = 0; j < m_capacity - i - 1; j++)
+        for(size_t j = 0; j < m_size - i - 1; j++)
         {
             if(m_data[j] > m_data[j + 1])
             {
@@ -207,49 +293,78 @@ void Vector<TYPE>::Sort()
 }
 
 template <class TYPE>
-void Vector<TYPE>::Clear()
+void Vector<TYPE>::clear()
 {
     delete[] m_data;
     m_data = nullptr;
     m_size = 0;
-    m_capacity = 0;
 }
 
 template <class TYPE>
-size_t Vector<TYPE>::GetSize() const
+void Vector<TYPE>::shrink_to_fit()
 {
-    return this->m_size;
+    if(m_capacity != m_size)
+    {
+        m_capacity = m_size;
+    }
 }
 
+template <class TYPE>
+void Vector<TYPE>::swap( Vector& other )
+{
+    size_t other_capacity = m_capacity;
+    size_t other_size = m_size;
+    TYPE* other_data = m_data;
+    if( this != &other )
+    {
+        copy( other );
+    }
+    other.m_capacity = other_capacity;
+    other.m_size = other_size;
+    other.m_data = other_data;
+
+}
 
 template <class TYPE>
-size_t Vector<TYPE>::GetCapacity() const
+TYPE& Vector<TYPE>::at(const size_t& pos) const
+{
+    return m_data[pos];
+}
+
+template <class TYPE>
+size_t Vector<TYPE>::size() const
+{
+    return m_size;
+}
+
+template <class TYPE>
+size_t Vector<TYPE>::capacity() const
 {
     return m_capacity;
 }
 
 template <class TYPE>
-bool Vector<TYPE>::IsEmpty() const
+bool Vector<TYPE>::empty() const
 {
     return m_size == 0;
 }
 
 template <class TYPE>
-TYPE& Vector<TYPE>::Front() const
+TYPE& Vector<TYPE>::front() const
 {
     return m_data[0];
 }
 
 template <class TYPE>
-TYPE& Vector<TYPE>::Back() const
+TYPE& Vector<TYPE>::back() const
 {
-    return m_data[m_capacity - 1];
+    return m_data[m_size - 1];
 }
 
 template <class TYPE>
-void Vector<TYPE>::Print() const
+void Vector<TYPE>::print() const
 {
-    for(size_t i = 0; i < m_capacity; i++)
+    for(size_t i = 0; i < m_size; i++)
     {
         cout << m_data[i] << " ";
     }
@@ -257,28 +372,25 @@ void Vector<TYPE>::Print() const
 
 // the private methods
 template <class TYPE>
-void Vector<TYPE>::Resize(const size_t& newSize)
+void Vector<TYPE>::resize(const size_t& newCapacity)
 {
-    m_size = newSize;
-    TYPE* newData = new TYPE[m_size];
-
-    for(size_t i = 0; i < m_capacity; i++)
+    m_capacity = newCapacity;
+    TYPE* newData = new TYPE[m_capacity];
+    for(size_t i = 0; i < m_size; i++)
     {
         newData[i] = m_data[i];
     }
-
     delete[] m_data;
     m_data = newData;
 }
 
 template <class TYPE>
-void Vector<TYPE>::Copy(const Vector &other)
+void Vector<TYPE>::copy(const Vector& other)
 {
-    m_size = other.m_size;
     m_capacity = other.m_capacity;
-    m_data = new TYPE[m_size];
-
-    for(size_t i = 0; i < m_capacity; i++)
+    m_size = other.m_size;
+    m_data = new TYPE[m_capacity];
+    for(size_t i = 0; i < m_size; i++)
     {
         m_data[i] = other.m_data[i];
     }
